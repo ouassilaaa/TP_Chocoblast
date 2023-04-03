f563a34d0397a91ecf4ea469338cb9c627c6d2b2
ouassilaaa <ouassidu3@hotmail.fr>
2023-04-03 16:55:09 +0200

    root

--- App/controler/ControlerAddUser.php
+++ App/controler/ControlerAddUser.php
@@ -1,24 +1,23 @@
 <?php
-
-include '.App/model/utilisateur.php';
-include '.App/controler/addUser.php'; 
+include '.App/model/utilisateur.php'; 
 include './App/utils/connectBdd.php'; 
-include './App/vue/view_add_user.php';
+include './App/view/view_add_user.php';
 
+$message = "message d'information";
 
 //tester si le formulaire est submit
 if(isset($_POST['submit'])){
     //tester si les champs sont remplis
-    if(!empty($_POST['nom']) AND !empty($_POST['prenom']) AND
-    !empty($_POST['mail'])AND !empty($_POST['image']) AND !empty($_POST['password'])){
+    if(!empty($_POST['nom_utilisateur']) AND !empty($_POST['prenom_utilisateur']) AND
+    !empty($_POST['mail_utilisateur'])AND !empty($_POST['image_utilisateur']) AND !empty($_POST['password_utilisateur'])){
         //nettoyage nom via function
         $nom_utilisateur = Fonction::cleandata($_POST['nom_utilisateur']);
         //stocker le contenu du formulaire
-        $nom = $_POST['nom'];
-        $prenom = $_POST['prenom'];
-        $mail = $_POST['mail'];
-        $image = $_POST['image'];
-        $password = $_POST['password'];
+        $nom = $_POST['nom_utilisateur'];
+        $prenom = $_POST['prenom_utilisateur'];
+        $mail = $_POST['mail_utilisateur'];
+        $image = $_POST['image_utilisateur'];
+        $password = $_POST['password_utilisateur'];
     }
     else{
         'Erreur formulaire.'; 
--- App/view/view_add_user.php
+++ App/view/view_add_user.php
@@ -1,24 +1,30 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
 
-<form action="../addUser.php" method="post" enctype="multipart/form-data">
+<div class="container">
+<form action="#" method="post" enctype="multipart/form-data">
         <p>Saisir le nom du fichier : </p>
-        <input type="text" name="nom">
+        <input type="text" name="nom_utilisateur">
         <p>Saisir l'adresse mail :</p>
-        <input type="email" name="mail">
+        <input type="email" name="mail_utilisateur">
         <p>Saisir password :</p>
-        <input type="password" name="password">
+        <input type="password" name="password_utilisateur">
         <p>Choisir un fichier : </p>
-        <input type="file" name="image">
+        <input type="file" name="image_utilisateur">
         <input type="submit" value="importer" name="submit">
     </form>
-    
+
+
+</div>   
 </body>
 </html>
