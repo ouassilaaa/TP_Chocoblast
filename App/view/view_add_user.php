94fd51325da606b17af88caee51c16cf871151ef
ouassilaaa <ouassidu3@hotmail.fr>
2023-04-04 16:13:33 +0200

    vue page inscription

--- App/view/view_add_user.php
+++ App/view/view_add_user.php
@@ -1,30 +1,51 @@
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
 </head>
 <body>
 
-<div class="container">
-<form action="#" method="post" enctype="multipart/form-data">
-        <p>Saisir le nom du fichier : </p>
-        <input type="text" name="nom_utilisateur">
-        <p>Saisir l'adresse mail :</p>
-        <input type="email" name="mail_utilisateur">
-        <p>Saisir password :</p>
-        <input type="password" name="password_utilisateur">
-        <p>Choisir un fichier : </p>
+  <div class="login-box">
+    <h2>Inscription &#129360</h2>
+    <form>
+      <div class="user-box">
+        <input type="text" name="nom_utilisateur" required="">
+        <label>Nom</label>
+    </div>
+
+    <div class="user-box">
+        <input type="email" name="mail_utilisateur" required="">
+        <label>Email</label>
+    </div>
+
+    <div class="user-box">
+        <input type="password" name="password_utilisateur" required="">
+        <label>Mot de passe </label>
+
+    </div>  
+    <div class="user-box">
+      <input type="confirm" name="confirm_password" required="">
+      <label>Confirmer le mot de passe</label>
+  </div>
+
+    <div class ="file">
         <input type="file" name="image_utilisateur">
-        <input type="submit" value="importer" name="submit">
+    </div>
+  
+      <a href="submit">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Je m'inscris
+      </a>
     </form>
-
-
-</div>   
+  </div>
 </body>
 </html>
+
