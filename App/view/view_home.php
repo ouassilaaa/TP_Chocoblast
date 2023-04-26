75584ed4eb6165acdd35a84e6503a19cc8a63299
ouassilaaa <ouassidu3@hotmail.fr>
2023-04-26 16:57:06 +0200

    css

--- App/view/view_home.php
+++ App/view/view_home.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Home page</title>
-    <link rel="stylesheet" href="/asset/style/style_home.css">
+    <link rel="stylesheet" href="./asset/style/style_home.css">
     
 </head>
 
@@ -15,66 +15,18 @@
             <ul class="navMenu__links">
                 <li class="navMenu__logo">
                     <a href="#">
-                        <img src="./asset/images/logoChocoblast.png" alt="Logo Chocoblast" />
+                        <img src="./asset/images/logobg.png" alt="Logo Chocoblast" />
                     </a>
                 </li>
-                <li><a href="#">Accueil</a></li>
-                <li><a href="#">Chocoblast</a></li>
-                <li><a href="#">Inscription</a></li>
-                <li><a href="#">Se connecter</a></li>
+                <li><a href="#">ACCUEIL</a></li>
+                <li><a href="#">CHOCOBLAST</a></li>
+                <li><a href="#">INSCRIPTION</a></li>
+                <li><a href="#">SE CONNECTER</a></li>
             </ul>
             <div class="dot"></div>
 
         </nav>
     </header>
 </body>
-<style>
-  @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap");
 
-* {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-
-    body {
-      background: #272727;
-      font-family: "Montserrat", sans-serif;
-    }
-
-    header {
-        background-color: whitesmoke;
-        
-    }
-
-    .navMenu {
-        padding: 0.2rem;
-    }
-
-    .navMenu__logo img {
-        max-height: 100px;
-        width: auto;
-    }
-
-    .navMenu__links {
-        list-style: none;
-        display: grid;
-        grid-template-columns: repeat(5, 1fr);
-        gap: 0.5rem;
-        place-items: center;
-    }
-
-    .navMenu__links a {
-        text-decoration: none;
-        color: #fff;
-        font-size: 1.1rem;
-        text-align: center;
-        
-    }
-
-    .navMenu__links a:hover{
-      color: blue;
-    }
-  
-</style>
 </html>
